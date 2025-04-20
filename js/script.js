590d3bcc42e6d36ed21bc1bb8ae406293432d406
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-20 03:04:20 +0000

    make hedings bolder

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 let hamburger_button = document.getElementById("hamburger-icon");
 let hamburger_menu = document.getElementById("hamburger-menu");
+const sections = document.querySelectorAll("section");
+console.log(sections);
 
+// TOGGLE HAMBURGER MENU
 hamburger_button.addEventListener("click", () => {
   toggleHamburgerMenu();
 });
 
 // toggle hamburger menu
 function toggleHamburgerMenu() {
-  console.log("opning...");
   hamburger_menu.classList.toggle("hamburger-open");
 }
+
+// SCROLL & REVEAL
